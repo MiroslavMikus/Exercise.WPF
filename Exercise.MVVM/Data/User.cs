3a3306a745b1228020cb298885b26564d1960225
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-01-30 21:44:12 +0100

    Improvements of MainViewModel

--- Exercise.MVVM/Data/User.cs
+++ Exercise.MVVM/Data/User.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GalaSoft.MvvmLight;
 
 namespace Exercise.MVVM.Data
 {
-    public class User
+    public class User : ObservableObject
     {
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
-        public int Age { get; set; }
+        private string _firstName;
+        public string FirstName { get => _firstName; set => Set(ref _firstName, value); }
+
+        private string _secondName;
+        public string SecondName { get => _secondName; set => Set(ref _secondName, value); }
+
+        private int _age;
+        public int Age { get => _age; set => Set(ref _age , value); }
     }
 }
--- Exercise.MVVM/ViewModel/MainViewModel.cs
+++ Exercise.MVVM/ViewModel/MainViewModel.cs
@@ -1,30 +1,33 @@
+using Exercise.MVVM.Data;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Exercise.MVVM.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
 
-        public MainViewModel()
+        public MainViewModel(IStorage storage)
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
-            Test = "Some test";
-            RaisePropertyChanged("Test");
+            var fakeData = storage.GetUsers();
+
+            AssignCommands();
+
+            Users = new ObservableCollection<User>(fakeData);
         }
 
+        public ObservableCollection<User> Users { get; set; }
 
-        private string _test;
-        public string Test
+        public ICommand Delete { get;  private set; }
+        public ICommand Create { get;  private set; }
+
+        private void AssignCommands()
         {
-            get => _test;
-            set => Set(ref _test, value);
+            Delete = new RelayCommand<User>(a => Users.Remove(a));
+
+            Create = new RelayCommand(() => Users.Add(new User { FirstName = "" });
         }
     }
 }
