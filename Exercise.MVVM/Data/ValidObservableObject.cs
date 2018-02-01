9b9d3f49beba8fb2bdd16cc8e6395bdaa994110d
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-02-01 20:12:31 +0100

    ValidObservable supports now multiline error messages

--- Exercise.MVVM/Data/ValidObservableObject.cs
+++ Exercise.MVVM/Data/ValidObservableObject.cs
@@ -1,8 +1,11 @@
 ﻿using FluentValidation;
 using FluentValidation.Internal;
+using FluentValidation.Results;
 using GalaSoft.MvvmLight;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Text;
 
 namespace Exercise.MVVM.Data
 {
@@ -23,12 +26,42 @@ namespace Exercise.MVVM.Data
 
                 var result = _validator.Validate(context);
 
-                return result.Errors.FirstOrDefault()?.ErrorMessage;
+                if (result.IsValid) return string.Empty;
+
+                return CreateErrorMessage(result.Errors);
             }
         }
 
-        private IValidator _validator;
+        private IValidator<T> _validator;
 
-        public string Error => "";
+        public bool IsValid { get { return _validator.Validate(this).IsValid; } }
+
+        public string Error
+        {
+            get
+            {
+                var result = _validator.Validate(this);
+                return CreateErrorMessage(result.Errors);
+
+            }
+        }
+
+        private string CreateErrorMessage(IList<ValidationFailure> errors)
+        {
+            // the property has only one error
+            if (errors.Count == 1)
+                return errors.FirstOrDefault()?.ErrorMessage;
+
+            // the property has multiple error
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var error in errors)
+            {
+                builder.AppendLine($"---- {errors.IndexOf(error) + 1 }. Message ----");
+                builder.AppendLine(error.ErrorMessage);
+            }
+
+            return builder.ToString();
+        }
     }
 }
