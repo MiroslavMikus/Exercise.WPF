348a0009edca49d6f025c60fb53125eb710de427
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-02-04 10:16:26 +0100

    Init prism exercise project

---
+++ Prism/Exercise.Prism/Exercise.Prism/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Exercise.Prism.ViewModel
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+
+            Update = new DelegateCommand(ExecuteUpdate, CanUpdate)
+                .ObservesProperty(() => FirstName)
+                .ObservesProperty(() => SecondName);
+        }
+
+        private bool CanUpdate()
+        {
+            return !String.IsNullOrEmpty(SecondName) &&
+                   !String.IsNullOrEmpty(FirstName);
+        }
+
+        private void ExecuteUpdate()
+        {
+            UpdatedTime = DateTime.Now;
+        }
+
+        public DelegateCommand Update { get; private set; }
+
+        private string _firstName = "John";
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { SetProperty(ref _firstName, value); }
+        }
+
+        private string _secondName;
+        public string SecondName
+        {
+            get { return _secondName; }
+            set { SetProperty(ref _secondName, value); }
+        }
+
+        private DateTime _dateTime;
+
+        public DateTime UpdatedTime
+        {
+            get { return _dateTime; }
+            set { SetProperty(ref _dateTime, value); }
+        }
+
+    }
+}
