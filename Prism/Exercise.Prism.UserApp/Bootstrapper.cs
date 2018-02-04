d8d3b41ca6ca345aab997595228db21957d9197b
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-02-04 12:15:17 +0100

    Add bootsrapper

---
+++ Prism/Exercise.Prism.UserApp/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Unity;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Exercise.Prism.User.Views;
+
+namespace Exercise.Prism.User
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
